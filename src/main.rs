07ba9ebd373152f90682f1692a16f33fdfd9f1c7
Caio Raposo <caioraposo@protonmail.com>
2020-11-22 17:49:27 -0300

    Add filename argument input

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use portugol::lexer::Lexer;
 use portugol::token::Token;
+use std::env;
+use std::fs;
 
 fn main() {
-    let input = include_str!("../para29");
+    let filename = env::args().nth(1).expect("Arquivo de input não inserido");
 
-    let mut lexer = Lexer::new(input.into());
+    let contents = fs::read_to_string(filename).expect("Erro ao ler arquivo");
+
+    let mut lexer = Lexer::new(contents.into());
 
     loop {
         let token = lexer.next_token();
         println!("{}", token);
         // Exit if End of File (EOF) is reached
-        if token.token == Token::Eof { break; }
+        if token.token == Token::Eof {
+            break;
+        }
     }
 }
